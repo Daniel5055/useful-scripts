fad2f088261480a6ee9a81dff20adc4ddee16ad8
Daniel Brathagen <db255@st-andrews.ac.uk>
2021-12-07 14:19:32 +0000

    Added insertion option for add and quiet option for most utilities Program more organised also with standard usage and error messages

--- todo.sh
+++ todo.sh
@@ -8,32 +8,47 @@
 # Syntax
 # First argument is necessary and must be either add, list, edit, or delete
 
+# Command name
+name="todo"
 # Functions
 usage ()
 {
     case "$1" in
         add|help)
-            echo "usage: $0 add <item>"
+            echo "usage: $name add [-q] [-i insertline] \"<item>\""
             ;;&
         edit|help)
-            echo "       $0 edit <item_number> <item>"
+            echo "usage: $name edit [-q] <item_number> \"<item>\""
             ;;&
         delete|help)
-            echo "       $0 delete <item_number>"
+            echo "usage: $name delete [-q] <item_number>"
             ;;&
         list|help)
-            echo "       $0 list"
+            echo "usage: $name list"
             ;;&
         help)
-            echo "       $0 help"
+            echo "usage: $name help"
             ;;
     esac
+}
+
+throw_error ()
+{
+    if [ "$#" -gt 1 ]; then
+        echo "$name: $2" 1>&2
+    fi
+    usage "$1"
+    exit 1
 }
 
 list ()
 {
     # Print all except last line which contains meta data
-    sed '$d' "$TODO_PATH"
+    if [ "$#" -eq 0 ]; then
+        sed '$d' "$TODO_PATH"
+    else
+        throw_error 'list'   
+    fi
 }
 
 # variable in script to be used by find item
@@ -47,31 +62,103 @@ find_item ()
     fi
 }
 
+# For renumbering the list
+renumber ()
+{
+    # Iterate through the list of all items
+    next=1
+    for l in $(grep -En '^\|\| [0-9]+)' "$TODO_PATH" | cut -d ":" -f 1); do
+        sed -Ei "$l"'s/[0-9]+/'"$next"'/' "$TODO_PATH"
+        next=$((next + 1))
+    done
+
+    # Finally fixed the bug... the inline option for sed should be the last option as it can take arguments
+}
+
 add ()
 {
-    if [ -n "$1" ]; then
+    is_insertion=0
+    is_quiet=0
+    # Check for options
+    while getopts "i:q" o; do
+        case "$o" in
+            i)
+                # Insert at number
+                is_insertion=1
+                insert_line="$OPTARG"
+                ;;
+            q)
+                is_quiet=1
+                ;;
+            *)
+                throw_error 'add'
+                ;;
+        esac
+    done
 
-        # If count zero then add first point, else add nth point dependent on count
+    shift $((OPTIND - 1))
+
+    # Check if item description provided and right number of arguments given
+    item=""
+    if [ "$#" -eq 1 ]; then
+        item="$1"
+    elif [ "$#" -eq 0 ]; then
+        item=$(cat -)
+    else
+        throw_error 'add'
+    fi
+
+    # If count zero then add first point, else add nth point dependent on count
+    if [ "$is_insertion" -eq 0 ]; then
         if [ "$count" -eq 0 ]; then
-            sed -i '/||/a\|| 1) '"$1"'\n||' "$TODO_PATH"
+            sed -i '/||/a\|| 1) '"$item"'\n||' "$TODO_PATH"
         else
             find_item "$count"
-            sed -i "$line"' a\||\n|| '$((count + 1))') '"$1" "$TODO_PATH"
+            sed -i "$line"' a\||\n|| '$((count + 1))') '"$item" "$TODO_PATH"
         fi
 
-        # Replace last line with new count
-        sed -i '$d' "$TODO_PATH"
-        echo $((count + 1)) >> "$TODO_PATH"
+    elif [[ "$insert_line" =~ ^[0-9]+$ ]]; then
 
+        # Check that number is not zero and is within range 
+        if [ "$insert_line" -gt 0 ] && [ "$insert_line" -le "$count" ]; then
+            find_item "$insert_line"
+            sed -i "$line"' i\|| '"$insert_line"') '"$item"'\n||' "$TODO_PATH"
+
+            # Then renumber
+            renumber
+        else
+            throw_error 'add' "line number not found"
+        fi
     else
-        usage 'add'
-        exit 1
+        throw_error 'add'
+    fi
+
+    # Replace last line with new count
+    sed -i '$d' "$TODO_PATH"
+    echo $((count + 1)) >> "$TODO_PATH"
+
+    if [ "$is_quiet" -eq 0 ]; then
+       list
     fi
 }
 
 edit ()
 {
-    if [ -n "$1" ] && [[ "$1" =~ ^[0-9]+$ ]] && [ -n "$2" ]; then
+    is_quiet=0
+    # Check for options
+    while getopts "q" o; do
+        case "$o" in
+            q)
+                is_quiet=1
+                ;;
+            *)
+                throw_error 'add'
+                ;;
+        esac
+    done
+
+    shift $((OPTIND - 1))
+    if [ "$#" -eq 2 ] && [[ "$1" =~ ^[0-9]+$ ]]; then
         find_item "$1"
 
         # If not found
@@ -85,24 +172,40 @@ edit ()
         usage 'edit'
         exit 1
     fi
+
+    if [ "$is_quiet" -eq 0 ]; then
+       list
+    fi
 }
 
 delete ()
 {
+    is_quiet=0
+    # Check for options
+    while getopts "q" o; do
+        case "$o" in
+            q)
+                is_quiet=1
+                ;;
+            *)
+                throw_error 'add'
+                ;;
+        esac
+    done
+
     # check that argument provided and is number
-    if [ -n "$1" ] && [[ "$1" =~ ^[0-9]+$ ]]; then
+    if [ "$#" -eq 1 ] && [[ "$1" =~ ^[0-9]+$ ]]; then
         find_item "$1"
 
         # If not found
         if [ "$line" -eq 0 ]; then
-            echo "Item not found"
-            exit 1
+            throw_error 'delete' "Line number not found"
         else
             sed -i "$line"','"$((line + 1 ))"'d' "$TODO_PATH"
         fi
 
         # And then renumber points :\
-        next=$1
+        next="$1"
         for ((i=$1+1; i <= count; i++)); do
             find_item "$i"
             sed -i "$line"'s/'"$i"'/'"$next"'/' "$TODO_PATH"
@@ -113,8 +216,11 @@ delete ()
         sed -i '$d' "$TODO_PATH"
         echo $((count - 1)) >> "$TODO_PATH"
     else
-        usage 'delete'
-        exit 1
+        throw_error 'delete'
+    fi
+
+    if [ "$is_quiet" -eq 0 ]; then
+       list
     fi
 }
 
@@ -150,25 +256,24 @@ count=$(tail -n 1 "$TODO_PATH")
 
 # First check that enough arguments provided
 if [ $# -ge 1 ];then
-    case $1 in
+    mode="$1"
+    shift
+    case "$mode" in
         add)
             # Add item
-            add "$2"
-            list
+            add "${@}"
             ;;
         edit)
             # Edit item
-            edit "$2" "$3"
-            list
+            edit "${@}"
             ;;
         delete)
             # Delete item
-            delete "$2"
-            list
+            delete "${@}"
             ;;
         list)
             # List items
-            list
+            list "${@}"
             ;;
         help|*)
             # Get Help
